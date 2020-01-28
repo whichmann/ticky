873710327e3de551dd64b0c53283b3cda21e9b66
Karol W <karol.wichmann@Outlook.com>
2020-01-28 23:21:31 +0100

    loader added

--- ticky/views.py
+++ ticky/views.py
@@ -1,5 +1,6 @@
 # Create your views here.
-from django.http import HttpResponse
+rom django.http import HttpResponse
+from django.template import loader
 from .models import Question
 
 def index(request):
