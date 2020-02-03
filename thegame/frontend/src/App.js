d776d61e762cec2d5b4fc75a630f46d950779d7d
Karol W <karol.wichmann@Outlook.com>
2020-02-04 00:01:47 +0100

    enough for today, store all set up, goodnight everyone

--- thegame/frontend/src/App.js
+++ thegame/frontend/src/App.js
@@ -2,17 +2,22 @@ import React, { useState } from "react";
 import Game from "./components/ticky/game";
 import Header from "./components/ticky/header";
 import Dashboard from "./components/users/dashboard";
+import { Provider } from "react-redux";
+import store from "./store";
+//TODO: configure webpack to integrate css files
 // import './App.css';
 
 function App() {
   return (
     <>
-      <Header></Header>
-      <br />
-      <div className="container">
-        <Gam />
-        <Dashboard />
-      </div>
+      <Provider store={store}>
+        <Header></Header>
+        <br />
+        <div className="container">
+          <Game />
+          <Dashboard />
+        </div>
+      </Provider>
     </>
   );
 }
