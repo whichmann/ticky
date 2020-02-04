74f7dac4359ab444cd081686addd3ea552ecebfb
Karol W <karol.wichmann@Outlook.com>
2020-02-04 17:42:45 +0100

    Added redux. Added fetching from users from the server. Styling changes

--- thegame/frontend/src/actions/users.js
+++ thegame/frontend/src/actions/users.js
@@ -1,1 +1,14 @@
-import 
+import axios from 'axios';
+import { GET_USERS } from './types';
+
+//GET_USERS action goes here:
+export const getUsers = () => dispatch => {
+    axios.get('/api/majority/')
+    .then(res => {
+        dispatch({
+            type: GET_USERS,
+            payload: res.data
+        })
+    })
+    .catch(err => console.log(err));
+}
